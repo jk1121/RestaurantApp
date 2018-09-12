13b2c1734506601ad64ba7285820c89ed1ff6286
Jatin Khattar 2 <jatkhatt0@publicisgroupe.net>
2018-09-12 23:55:14 +0530

    Angular components part-2 :  Material Button ,Material Card and if structural directive implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import {MatToolbarModule,MatListModule} from '@angular/material';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
 import 'hammerjs';
@@ -15,7 +18,11 @@ import { MenuComponent } from './menu/menu.component';
     MenuComponent
   ],
   imports: [
+   
+    MatCardModule,
+    MatButtonModule,
     MatListModule,
+    MatGridListModule,
     MatToolbarModule,
     BrowserAnimationsModule,
     BrowserModule,
