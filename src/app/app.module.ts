3e67fe95580e76f574eeb767c915578de39607e4
Jatin Khattar 2 <jatkhatt0@publicisgroupe.net>
2018-10-18 22:39:06 +0530

    Implemented basic service to fetch data from dishes object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import { AppComponent } from './app.component';
-import {MatToolbarModule,MatListModule} from '@angular/material';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {HttpModule} from '@angular/http';
-import {FormsModule} from '@angular/forms';
-import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppComponent } from "./app.component";
+import { MatToolbarModule, MatListModule } from "@angular/material";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
+import "hammerjs";
+import { MenuComponent } from "./menu/menu.component";
+import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { DishService } from "../app/services/dish.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    DishdetailComponent
-  ],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent],
   imports: [
-   
     MatCardModule,
     MatButtonModule,
     MatListModule,
@@ -31,9 +27,8 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FlexLayoutModule,
     HttpModule,
     FormsModule
-    
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
